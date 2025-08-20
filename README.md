0071638c98db18d60ac7c4183882f72a5aad0403
Denis Merezhin <denis.merezhin@evraz.com>
2025-08-20 23:28:46 +0300

    авто изменение названия веб части

--- README.md
+++ README.md
@@ -1,6 +1,7 @@
 # SPFx Boilerplate
 
-Boilerplate for making SPFx project for SharePoint 2019 with updated versions of key component (React, TypeScript) and DI container.
+Boilerplate for making SPFx project for SharePoint 2019 with updated versions of key component (React, TypeScript) and
+DI container.
 
 ### Usage
 
@@ -10,18 +11,22 @@ npm run preinstall
 npm install
 ```
 
-If you use windows OS run 
+If you use windows OS run
+
 ```bash
 npm run fixbuildw
 ```
 
-for Linux/MacOS run 
+for Linux/MacOS run
+
 ```bash
 npm run fixbuildu
 ```
+
 This command you need to run always after package installation for changing version of TypeScript from 2 to 4.
 
 To rename your project run
+
 ```bash
 npm run-script config <YourName>
 ```
@@ -39,6 +44,9 @@ This package produces the following:
 * gulp serve - Run for debugging (commonly for using with --nobrowser key and connecting debugger from vs code)
 * gulp bundle - Make bundle (for production with --ship param)
 * gulp package-solution - Make SPFx package (for production with --ship param)
+
+* npm run start:react - Run React components
+* npm run build:react - Build React components
 
 ###Issues
 roundedCorner2 Error& Add this line to webpart constructor: ```loadTheme({effects:{roundedCorner2: '0px'}})``` 
--- configProject.js
+++ configProject.js
@@ -1,49 +1,51 @@
 const fs = require('fs');
 const projectName = process.argv[2];
-const rootDir = 'src/webparts/HelloWorld'
+const rootDir = 'src/webparts/HelloWorld';
+const rootReact = 'src/react-app/src';
 
 const renameInsideFiles = [
-  'config/config.json',
-  `${rootDir}/components/HelloWorld/HelloWorld.tsx`,
-  `${rootDir}/container/HelloWorldContainer.tsx`,
-  `${rootDir}/loc/mystrings.d.ts`,
-  `${rootDir}/HelloWorldWebPart.manifest.json`,
-  `${rootDir}/HelloWorldWebPart.ts`,
-  `${rootDir}/components/index.ts`
+    'config/config.json',
+    `${rootDir}/components/HelloWorld/HelloWorld.tsx`,
+    `${rootDir}/container/HelloWorldContainer.tsx`,
+    `${rootDir}/loc/mystrings.d.ts`,
+    `${rootDir}/HelloWorldWebPart.manifest.json`,
+    `${rootDir}/HelloWorldWebPart.ts`,
+    `${rootDir}/components/index.ts`,
+    `${rootReact}/App.tsx`
 ];
 
 const renameFilesList = [
-  [`${rootDir}/HelloWorldWebPart.ts`, `${rootDir}/${projectName}WebPart.ts`],
-  [`${rootDir}/HelloWorldWebPart.manifest.json`, `${rootDir}/${projectName}WebPart.manifest.json`],
-  [`${rootDir}/container/HelloWorldContainer.tsx`, `${rootDir}/container/${projectName}Container.tsx`],
-  [`${rootDir}/components/HelloWorld/HelloWorld.tsx`, `${rootDir}/components/HelloWorld/${projectName}.tsx`],
-  [`${rootDir}/components/HelloWorld`, `${rootDir}/components/${projectName}`],
-  [`${rootDir}`, `src/webparts/${projectName}`],
+    [`${rootDir}/HelloWorldWebPart.ts`, `${rootDir}/${projectName}WebPart.ts`],
+    [`${rootDir}/HelloWorldWebPart.manifest.json`, `${rootDir}/${projectName}WebPart.manifest.json`],
+    [`${rootDir}/container/HelloWorldContainer.tsx`, `${rootDir}/container/${projectName}Container.tsx`],
+    [`${rootDir}/components/HelloWorld/HelloWorld.tsx`, `${rootDir}/components/HelloWorld/${projectName}.tsx`],
+    [`${rootDir}/components/HelloWorld`, `${rootDir}/components/${projectName}`],
+    [`${rootDir}`, `src/webparts/${projectName}`],
 ];
 
 const renameNames = [
-  ['helloworld-web-part', `${projectName.toLocaleLowerCase()}-web-part`],
-  [/HelloWorld/g, `${projectName}`],
-  ['Hello World', `${projectName}`]
+    ['helloworld-web-part', `${projectName.toLocaleLowerCase()}-web-part`],
+    [/HelloWorld/g, `${projectName}`],
+    ['Hello World', `${projectName}`]
 ]
 
 const renameInsideFileSync = () => {
-  for (const filePath of renameInsideFiles) {
-     let file = fs.readFileSync(filePath, 'utf8');
-     for (const rename of renameNames) {
-      file = file.replace(rename[0], rename[1]);
-    }
+    for (const filePath of renameInsideFiles) {
+        let file = fs.readFileSync(filePath, 'utf8');
+        for (const rename of renameNames) {
+            file = file.replace(rename[0], rename[1]);
+        }
 
-    fs.writeFileSync(filePath, file);
-    console.log(`Сontent overwritten in file ${filePath}`)
+        fs.writeFileSync(filePath, file);
+        console.log(`Сontent overwritten in file ${filePath}`)
     }
 }
 
 const renameFilesSync = () => {
-for (const filePath of renameFilesList) {
-  fs.renameSync(filePath[0], filePath[1]);
-  console.log(`File renamed from ${filePath[0]} to ${filePath[1]}`)
-}
+    for (const filePath of renameFilesList) {
+        fs.renameSync(filePath[0], filePath[1]);
+        console.log(`File renamed from ${filePath[0]} to ${filePath[1]}`)
+    }
 }
 
 renameInsideFileSync();
